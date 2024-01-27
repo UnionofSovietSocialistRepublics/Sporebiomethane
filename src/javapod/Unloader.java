97e7d8a0ab4fcefc9638477b6523e27b8a78c637
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-01-27 10:32:48 +0700

    work out

---
+++ src/javapod/Unloader.java
@@ -0,0 +1,24 @@
+package javapod;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class Unloader extends Mod{
+
+    public Unloader(){
+        Log.info("The fog the fog is coming");
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Go to hell.");
+        JpodUnitTypes.load();
+    }
+
+}
