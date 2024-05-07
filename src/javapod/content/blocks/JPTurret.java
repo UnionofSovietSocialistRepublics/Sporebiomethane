5df45205c66da308003daa410f3053bbc04903e7
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-05-07 22:04:12 +0700

    ddraw

--- src/javapod/content/blocks/JPTurret.java
+++ src/javapod/content/blocks/JPTurret.java
@@ -127,17 +127,12 @@ Impalercannon,Flakaccelerator,Artilleryturret,Oarch,Sporegarrison,Mitocannon,Sup
                 }});
                 coolant = consumeCoolant(0.1f);
                 drawer = new DrawTurret(){{
-                parts.add(new RegionPart("-canright"){{
+                parts.add(new RegionPart("-canleft"){{
+                    mirror = true;
                     progress = PartProgress.recoil;
                     under = true;
                     moveY = -4f;
-                    moveX = -4f;
-                }});
-                parts.add(new RegionPart("-canleft"){{
-                    progress = PartProgress.recoil;
-                    under = true;
-                    moveY = 4f;
-                    moveX = 4f;
+                    moveRot = 10f;
                 }});
             }};
         }};
