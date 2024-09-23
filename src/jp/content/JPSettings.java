e6f112327b9bd8f1110c63e6b72500740b09f07b
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-09-23 21:59:48 +0700

    AmbautuRAAAAAAAH

--- src/jp/content/JPSettings.java
+++ src/jp/content/JPSettings.java
@@ -9,7 +9,7 @@ public class JPSettings {
     public static void init() {
         //Each you turn on or off those setting you need to restart the game for it to take effect. (They're not by default.)
         Vars.ui.settings.addCategory(Core.bundle.get("jpsettings"), "jp-sporeset", jp -> {
-            jp.checkPref("jp-HM", false);
+            jp.checkPref("HM", false);
         });
 
     }
