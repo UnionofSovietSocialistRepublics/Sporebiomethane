5123d48007f51ce258457f302b452ce588c4c1f1
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-09-29 20:34:35 +0700

    mmmmmm

--- src/jp/content/JPUnits.java
+++ src/jp/content/JPUnits.java
@@ -328,10 +328,10 @@ public class JPUnits {
                 }};
             }});
             outlineColor = Color.valueOf("303a45");
-            abilities.add(new MoveEffectAbility(-3f, -7f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
+            abilities.add(new MoveEffectAbility(-5f, -7f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
                 teamColor = true;
             }});
-            abilities.add(new MoveEffectAbility(3f, -7f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
+            abilities.add(new MoveEffectAbility(5f, -7f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
                 teamColor = true;
             }});
             abilities.add(new RegenAbility(){{percentAmount = 1f / (120f * 60f * 2f) * 100f;}});
@@ -460,10 +460,10 @@ public class JPUnits {
             }});
             healColor = Color.valueOf("590e14");
             outlineColor = Color.valueOf("303a45");
-            abilities.add(new MoveEffectAbility(-3f, -3f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
+            abilities.add(new MoveEffectAbility(-5f, -3f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
                 teamColor = true;
             }});
-            abilities.add(new MoveEffectAbility(3f, -3f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
+            abilities.add(new MoveEffectAbility(5f, -3f, Pal.sapBulletBack, Fx.missileTrailShort, 4f){{
                 teamColor = true;
             }});
             abilities.add(new RegenAbility(){{percentAmount = 1f / (70f * 60f * 2f) * 100f;}});
