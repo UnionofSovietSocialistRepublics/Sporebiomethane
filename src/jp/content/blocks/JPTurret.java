e9935c64874af1ca5acdd22fccfc1f0c0b99a09e
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-10-03 09:17:31 +0700

    ok what how

--- src/jp/content/blocks/JPTurret.java
+++ src/jp/content/blocks/JPTurret.java
@@ -104,6 +104,9 @@ public class JPTurret{
             shoot.shots = 2;
             shoot.shotDelay = 10f;
             inaccuracy = 2f;
+            shoot = new ShootAlternate(){{
+                barrels = 2;
+            }};
             ammo(
                 sporePod, new FlakBulletType(5f, 450f){{
                     lifetime = 65f;
