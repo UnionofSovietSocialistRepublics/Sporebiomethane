26ab60f8c7b64807214810daed58d3530b975b82
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-04-02 16:43:11 +0700

    hh

--- src/jp/content/JPUnits.java
+++ src/jp/content/JPUnits.java
@@ -885,8 +885,8 @@ public class JPUnits {
             drag = 0.05f;
             accel = 0.11f;
             weapons.add(new Weapon("Ambutakam"){{
-                minShootVelocity = 0.75f;
-                x = 1;
+                minShootVelocity = 0.25f;
+                x = 2;
                 y = 0;
                 shootY = 0f;
                 reload = 10f;
