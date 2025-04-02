e1bfa941e8c45d4f0beb096881c0a2a94d1fc81d
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-04-02 17:33:55 +0700

    666666

--- src/jp/content/JPStatus.java
+++ src/jp/content/JPStatus.java
@@ -4,7 +4,7 @@ import mindustry.type.StatusEffect;
 
 public class JPStatus {
     public static StatusEffect
-    Dissolving, Frostshield, Regen, Mindcontrol, Reversion, ReversionD;
+    Dissolving, Frostshield, Regen, Mindcontrol, Reversion, ReversionD, Hellspawn;
     public static void load(){
         Dissolving = new StatusEffect("Dissolving"){{
             damage = 60f / 60f;
@@ -21,5 +21,10 @@ public class JPStatus {
         ReversionD = new StatusEffect("ReversionD"){{
             speedMultiplier = -1f;
             disarm = true;
+        }};
+        Hellspawn = new StatusEffect("Hellspawn"){{
+           damage = 6f / 60f;
+           speedMultiplier = 1.25f;
+           damageMultiplier = 1.25f;
         }};
     }};
