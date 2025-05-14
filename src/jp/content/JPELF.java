67a2e8312ac6f67e7348f01c151de107a003cdb3
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-05-14 23:03:27 +0700

    what coukld possibly go wron

--- src/jp/content/JPELF.java
+++ src/jp/content/JPELF.java
@@ -1,5 +1,17 @@
 package jp.content;
 
+import arc.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.content.*;
+import mindustry.entities.*;
+import mindustry.game.EventType.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.consumers.*;
+import mindustry.world.meta.*;
 import mindustry.world.blocks.production.*;
 
 /** Factory with explodeOnFull because Anuke haven't added explodeonfull to factories yet */
@@ -8,5 +20,23 @@ public class JPELF extends GenericCrafter{
 
     public JPELF(String name) {
         super(name);
+    }
+
+    @Override
+        public void init(){
+            if(explodeOnFull && outputLiquid != null && explosionPuddleLiquid == null){
+                explosionPuddleLiquid = outputLiquid.liquid;
+            }
+            emitLight = true;
+            lightRadius = baseLightRadius * size;
+            super.init();
+        }
+
+    @Override
+    public void updateTile(){
+            if(explodeOnFull && liquids.get(outputLiquid.liquid) >= liquidCapacity - 0.01f){
+                kill();
+                Events.fire(new GeneratorPressureExplodeEvent(this));
+            }
     }
 }
