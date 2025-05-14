18d3ae2e1b61c6c968ce7a0c59ccca8005a0d6b9
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-05-14 22:37:43 +0700

    Please work

--- src/jp/content/JPELF.java
+++ src/jp/content/JPELF.java
@@ -1,16 +1,10 @@
 package jp.content;
 
 import arc.*;
-import arc.graphics.*;
 import arc.math.*;
-import arc.struct.*;
-import arc.util.*;
-import mindustry.content.*;
-import mindustry.entities.*;
-import mindustry.game.EventType.*;
 import mindustry.graphics.*;
-import mindustry.type.*;
-import mindustry.world.consumers.*;
+import mindustry.ui.*;
+import mindustry.world.blocks.heat.*;
 import mindustry.world.meta.*;
 
 /** Factory with explodeOnFull because anuke havent add explodeonfull to factories yet */
