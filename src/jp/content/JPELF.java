c6c24545f813a7596b0e6e0df96bda92706c03a1
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-05-14 23:14:01 +0700

    Work, now.

--- src/jp/content/JPELF.java
+++ src/jp/content/JPELF.java
@@ -14,29 +14,37 @@ import mindustry.world.consumers.*;
 import mindustry.world.meta.*;
 import mindustry.world.blocks.production.*;
 
+import static mindustry.Vars.tilesize;
+
 /** Factory with explodeOnFull because Anuke haven't added explodeonfull to factories yet */
-public class JPELF extends GenericCrafter{
+public class JPELF extends GenericCrafter {
     public boolean explodeOnFull = false;
+    public float baseLightRadius = 65f;
+    public @Nullable Liquid explosionPuddleLiquid;
 
     public JPELF(String name) {
         super(name);
     }
 
     @Override
-        public void init(){
-            if(explodeOnFull && outputLiquid != null && explosionPuddleLiquid == null){
-                explosionPuddleLiquid = outputLiquid.liquid;
-            }
-            emitLight = true;
-            lightRadius = baseLightRadius * size;
-            super.init();
+    public void init() {
+        if (explodeOnFull && outputLiquid != null && explosionPuddleLiquid == null) {
+            explosionPuddleLiquid = outputLiquid.liquid;
         }
+        emitLight = true;
+        lightRadius = baseLightRadius * size;
+        super.init();
+    }
 
-    @Override
-    public void updateTile(){
-            if(explodeOnFull && liquids.get(outputLiquid.liquid) >= liquidCapacity - 0.01f){
+
+    public class JPELFBuild extends GenericCrafterBuild {
+
+        @Override
+        public void updateTile() {
+            if (explodeOnFull && liquids.get(outputLiquid.liquid) >= liquidCapacity - 0.01f) {
                 kill();
                 Events.fire(new GeneratorPressureExplodeEvent(this));
             }
+        }
     }
 }
