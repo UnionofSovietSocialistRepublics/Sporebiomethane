9c0f97343d222eda4799f5ad8a7ebb091c23f55c
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-05-16 10:20:52 +0700

    pregnant man emoji

--- src/jp/content/JPBoostableCG.java
+++ src/jp/content/JPBoostableCG.java
@@ -3,25 +3,7 @@ package jp.content;
 import mindustry.world.blocks.power.*;
 import mindustry.world.consumers.*;
 import mindustry.world.meta.*;
-import arc.*;
-import arc.graphics.*;
-import arc.graphics.g2d.*;
-import arc.math.*;
-import arc.struct.*;
-import arc.util.*;
-import arc.util.io.*;
-import mindustry.annotations.Annotations.*;
-import mindustry.content.*;
-import mindustry.entities.*;
-import mindustry.entities.units.*;
-import mindustry.game.*;
-import mindustry.gen.*;
-import mindustry.graphics.*;
-import mindustry.logic.*;
-import mindustry.type.*;
-import mindustry.ui.*;
-import mindustry.world.*;
-import mindustry.world.blocks.environment.*;
+
 
 public class JPBoostableCG extends ConsumeGenerator {
 
@@ -32,21 +14,25 @@ public class JPBoostableCG extends ConsumeGenerator {
         super(name);
     }
 
+    @Override
+    public void setStats() {
+        super.setStats();
+        stats.timePeriod = useTime;
+
+
+        if(liquidBoostIntensity != 1 && findConsumer(f -> f instanceof ConsumeLiquidBase && f.booster) instanceof ConsumeLiquidBase consBase){
+            stats.remove(Stat.booster);
+            stats.add(Stat.booster,
+                    StatValues.speedBoosters("{0}" + StatUnit.timesSpeed.localized(),
+                            consBase.amount,
+                            liquidBoostIntensity * liquidBoostIntensity, false, consBase::consumes)
+            );
+        }
+    }
+
     public class JPBoostableCGBuild extends ConsumeGeneratorBuild{
 
-        public void setStats() {
-            stats.timePeriod = useTime;
 
-
-            if(liquidBoostIntensity != 1 && findConsumer(f -> f instanceof ConsumeLiquidBase && f.booster) instanceof ConsumeLiquidBase consBase){
-                stats.remove(Stat.booster);
-                stats.add(Stat.booster,
-                        StatValues.speedBoosters("{0}" + StatUnit.timesSpeed.localized(),
-                                consBase.amount,
-                                liquidBoostIntensity * liquidBoostIntensity, false, consBase::consumes)
-                );
-            }
-        }
         }
     }
 
