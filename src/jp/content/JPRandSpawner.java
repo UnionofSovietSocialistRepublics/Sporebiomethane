57ff171caac83100de5c05069e2d2db727b60547
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2026-02-17 17:36:12 +0700

    schizopenria

--- src/jp/content/JPRandSpawner.java
+++ src/jp/content/JPRandSpawner.java
@@ -50,18 +50,16 @@ public class JPRandSpawner extends Ability{
 
     @Override
     public void update(Unit unit){
-        // Should randomize between zero and one
-        // float x;
         Unit u;
         rand = (int)Math.random()*5;
         timer += Time.delta * state.rules.unitBuildSpeed(unit.team);
 
         if(timer >= spawnTime){
             float x = unit.x + Angles.trnsx(unit.rotation, spawnY, -spawnX), y = unit.y + Angles.trnsy(unit.rotation, spawnY, -spawnX);
-            if(Units.canCreate(unit.team, this.unit)&&rand<=0){
+            if(Units.canCreate(unit.team, this.unit)&&(unit.team.data().countType(this.unit)<unit.team.data().countType(this.altUnit))){
                 
                 spawnEffect.at(x, y, 0f, parentizeEffects ? unit : null);
-                u = this.altUnit.create(unit.team);
+                u = this.unit.create(unit.team);
                 u.rotation = unit.rotation;
                 u.set(x, y);
                 Events.fire(new UnitCreateEvent(u, null, unit));
@@ -72,7 +70,7 @@ public class JPRandSpawner extends Ability{
 
                 timer = 0f;
             }
-            else if(Units.canCreate(unit.team, this.altUnit)&&rand>=1){
+            else if(Units.canCreate(unit.team, this.altUnit)){
                 spawnEffect.at(x, y, 0f, parentizeEffects ? unit : null);
                 u = this.altUnit.create(unit.team);
                 u.rotation = unit.rotation;
